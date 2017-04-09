4ff1103c197c3d3d20f1d92bc5334531e2c0ad87
Scott Dover <sdover102@gmail.com>
2017-04-09 15:35:55 -0400

    🐛  Built site in  instead of

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const env = process.env.NODE_ENV || 'production';
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const buildPath = path.resolve(__dirname, '_site', 'dist');
+const buildPath = path.resolve(__dirname, 'docs', 'dist');
 const assetsPath = path.resolve(__dirname, 'app', 'assets');
 
 const babelOptions = {
